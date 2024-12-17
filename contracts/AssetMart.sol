82c543ef86d2bdafe5ab15049da5170d7fa28526
Om Swaroop Nayak <146124307+itoshi69sae@users.noreply.github.com>
2024-12-17 21:48:29 +0530

    Update and rename Lock.sol to AssetMart.sol

---
+++ contracts/AssetMart.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract AssetMart is ERC721URIStorage {
+    constructor() ERC721("AssetMart", "AM") {
+        owner = payable(msg.sender);
+    }
+}
