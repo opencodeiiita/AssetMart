69bba299da50078cd707eb29dbdbe935f416f615
Ritesh N Das <148974606+rndastech@users.noreply.github.com>
2024-12-19 14:18:44 +0530

    Update asset.sol

--- Task2/rndastech/asset.sol
+++ Task2/rndastech/asset.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract AssetMart is ERC721URIStorage, Ownable, ReentrancyGuard {
+contract AssetMart is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
-    uint256 public mintingCap = 10000;
     uint256 public fee = 0.01 ether;
 
     struct Item {
@@ -22,16 +20,14 @@ contract AssetMart is ERC721URIStorage, Ownable, ReentrancyGuard {
 
     event TokenMinted(address to, uint256 tokenId);
     event ListingSuccess(uint256 indexed id, address owner, address seller, uint256 price, bool listed);
-    event TransferSuccess(uint256 indexed id, address from, address to);
 
     constructor() ERC721("AssetMart", "AM") {
         nextTokenId = 1;
     }
 
-    function createToken(string memory uri, uint256 price) external payable nonReentrant {
+    function createToken(string memory uri, uint256 price) external payable {
         require(msg.value == fee, "Fee required");
         require(price > 0, "Invalid price");
-        require(nextTokenId <= mintingCap, "AssetMart: Minting cap reached");
 
         uint256 tokenId = nextTokenId;
         _safeMint(msg.sender, tokenId);
@@ -39,28 +35,16 @@ contract AssetMart is ERC721URIStorage, Ownable, ReentrancyGuard {
         nextTokenId++;
 
         _transfer(msg.sender, address(this), tokenId);
-        emit TransferSuccess(tokenId, msg.sender, address(this));
-
-        _addItem(tokenId, price);
-
-        emit ListingSuccess(tokenId, address(this), msg.sender, price, true);
-    }
-
-    function _addItem(uint256 id, uint256 price) private {
-        items[id] = Item({
-            id: id,
+        
+        items[tokenId] = Item({
+            id: tokenId,
             price: price,
             owner: payable(address(this)),
             seller: payable(msg.sender),
             listed: true
         });
-    }
 
-    function setFee(uint256 newFee) external onlyOwner {
-        fee = newFee;
-    }
-
-    function getItem(uint256 id) external view returns (Item memory) {
-        return items[id];
+        emit TokenMinted(msg.sender, tokenId);
+        emit ListingSuccess(tokenId, address(this), msg.sender, price, true);
     }
 }
