3401405d566cecbf700a135ce07ab3ea9389e6a9
Om Swaroop Nayak <146124307+itoshi69sae@users.noreply.github.com>
2024-12-23 11:36:01 +0530

    Update AssetMart.sol

--- contracts/AssetMart.sol
+++ contracts/AssetMart.sol
@@ -9,4 +9,60 @@ contract AssetMart is ERC721URIStorage {
     constructor() ERC721("AssetMart", "AM") {
         owner = payable(msg.sender);
     }
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+    address payable owner;
+    uint256 listPrice = 0.01 ether;
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;
+    }
+    event TokenListedSuccess (
+        uint256 indexed tokenId,
+        address owner,
+        address seller,
+        uint256 price,
+        bool currentlyListed
+    );
+    mapping(uint256 => ListedToken) private idToListedToken;
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId);
+
+        _setTokenURI(newTokenId, tokenURI);
+
+        createListedToken(newTokenId, price);
+
+        return newTokenId;
+    }
+
+    function createListedToken(uint256 tokenId, uint256 price) private {
+        require(msg.value == listPrice, "Hopefully sending the correct price");
+        require(price > 0, "Make sure the price isn't negative");
+
+        idToListedToken[tokenId] = ListedToken(
+            tokenId,
+            payable(address(this)),
+            payable(msg.sender),
+            price,
+            true
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+        emit TokenListedSuccess(
+            tokenId,
+            address(this),
+            msg.sender,
+            price,
+            true
+        );
+    }
+
 }
