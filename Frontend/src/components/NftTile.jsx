7cba6229395571ba3021ed71d80a14cf638b2744
Darkknight0125 <samansidhu0125@gmail.com>
2025-01-07 22:22:14 +0530

    Retask 9

--- Frontend/src/components/NftTile.jsx
+++ Frontend/src/components/NftTile.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import './NftTile.css';
 
-const NftTile = ({ image, name, description }) => {
+const NFTTile = ({ nft, onBuy }) => {
   return (
     <div className="nft-tile">
-      <img src={image} alt={name} className="nft-image" />
-      <div className="nft-details">
-        <h3 className="nft-name">{name}</h3>
-        <p className="nft-description">{description}</p>
-      </div>
+      <img src={nft.image} alt={nft.name} className="nft-image" />
+      <h2>{nft.name}</h2>
+      <p>Price: {nft.price} ETH</p>
+      <p>Seller: {nft.seller}</p>
+      <button onClick={() => onBuy(nft)}>Buy this NFT</button>
     </div>
   );
-};
+}
 
-export default NftTile;
+export default NFTTile;
